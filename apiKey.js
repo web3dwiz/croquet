3257d48acffdae699edc1c1745671cff141dcaf8
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2022-08-29 15:36:53 -0400

    add api key

---
+++ apiKey.js
@@ -0,0 +1,5 @@
+const apiKey = "1CatLy2uG1tICgbAsH62Gvb92xMHub6nrvBNMrxOq";
+const appId = "io.sweet.yanling.pang.microverse";
+export default {apiKey, appId};
+
+// you may export other Croquet session parameters to override default values.
